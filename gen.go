242797eeb0a866ea0128589b4c545e2b78ff6877
Eric Paris <eparis@redhat.com>
2015-03-11 16:35:47 -0400

    create a command structure

--- gen.go
+++ gen.go
@@ -3,8 +3,22 @@ package main
 import (
 	"github.com/spf13/cobra"
 )
+
+func cmd(name string) *cobra.Command {
+	return &cobra.Command{
+		Use: name,
+	}
+}
+
 func main() {
-	cmd := &cobra.Command{}
-	cmd.Use = "hello"
+	l1 := cmd("1")
+	l2a := cmd("2a")
+	l2b := cmd("2b")
+	l2a1 := cmd("2a1")
+
+	l2a.AddCommand(l2a1)
+	l1.AddCommand(l2a)
+	l1.AddCommand(l2b)
+
 	return
 }
